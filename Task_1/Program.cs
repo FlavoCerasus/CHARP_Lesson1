5bac9c2647d32f59cce5f74e751b35175a548160
Danil <eliseev.danii@yandex.ru>
2024-04-23 19:31:17 +0300

    add some variables lesso1:task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World!");
+﻿// Console.WriteLine("Hello World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
