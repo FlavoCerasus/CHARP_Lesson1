db222139f429f90d6dfa7aee340db87ad8439fa1
Danil <eliseev.danii@yandex.ru>
2024-04-23 20:24:26 +0300

    add lesson1:task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 7;
+int c = 10;
+int d = 2;
+int e = 5;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
